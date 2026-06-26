6912b3ac4339ae06adab1262326b01d760d5f01c
willer36 <6400910@bdeb.qc.ca>
2026-06-26 12:00:54 -0400

    mon premier pipeline CI

--- tests/test_connexion.py
+++ tests/test_connexion.py
@@ -31,9 +31,9 @@ def test_login(driver):
     #d'environnement
     # Si APP_URL n'est pas fournie par GitHub, on utilise l'IP publique de
     #votre Redmine
-    url = os.environ.get("APP_URL", "APP_URL")
-    username = os.environ.get("APP_USERNAME", "APP_USERNAME")
-    password = os.environ.get("APP_PASSWORD", "APP_PASSWORD")
+    url = os.environ.get("APP_URL")
+    username = os.environ.get("APP_USERNAME")
+    password = os.environ.get("APP_PASSWORD")
     driver.get(url + "/login")
     driver.find_element(By.ID, "username").send_keys(username)
     driver.find_element(By.ID, "password").send_keys(password)
